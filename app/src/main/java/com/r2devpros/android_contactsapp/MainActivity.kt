5544f9ba92f8d31fa2f2f7a2a3f37556e7740a5f
ascstb <ascstb@gmail.com>
2022-05-26 12:28:51 -0500

    Enabling repo interaction

--- app/src/main/java/com/r2devpros/android_contactsapp/MainActivity.kt
+++ app/src/main/java/com/r2devpros/android_contactsapp/MainActivity.kt
@@ -2,10 +2,27 @@ package com.r2devpros.android_contactsapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.r2devpros.android_contactsapp.repository.RepositoryManager
+import com.r2devpros.android_contactsapp.repository.local.room.ContactsRepo
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var repositoryManager: RepositoryManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        repositoryManager = RepositoryManager(
+            ContactsRepo(this)
+        )
+
+        CoroutineScope(Dispatchers.IO).launch {
+            val result = repositoryManager.getContacts()
+            Timber.d("MainActivity_TAG: onCreate: contacts: ${result.size}")
+        }
     }
 }
--- app/src/main/java/com/r2devpros/android_contactsapp/repository/RepositoryManager.kt
+++ app/src/main/java/com/r2devpros/android_contactsapp/repository/RepositoryManager.kt
@@ -1,4 +1,20 @@
 package com.r2devpros.android_contactsapp.repository
 
-class RepositoryManager {
+import com.r2devpros.android_contactsapp.model.Contact
+import com.r2devpros.android_contactsapp.repository.local.room.ContactsRepo
+
+class RepositoryManager(
+    private val contactsRepo: ContactsRepo
+) {
+    fun getContacts(): List<Contact> {
+        return contactsRepo.getContacts()
+    }
+
+    fun addContact(contact: Contact) {
+        return contactsRepo.addContact(contact)
+    }
+
+    fun deleteContactById(contactId: String) {
+        return contactsRepo.deleteContactById(contactId)
+    }
 }
