7737698a6b11e828f384c0747fe8212986d0b6d3
ascstb <ascstb@gmail.com>
2022-05-26 12:38:26 -0500

    Enabling add to database from form

--- app/src/main/java/com/r2devpros/android_contactsapp/MainActivity.kt
+++ app/src/main/java/com/r2devpros/android_contactsapp/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.r2devpros.android_contactsapp
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.textfield.TextInputEditText
+import com.r2devpros.android_contactsapp.model.Contact
 import com.r2devpros.android_contactsapp.repository.RepositoryManager
 import com.r2devpros.android_contactsapp.repository.local.room.ContactsRepo
 import kotlinx.coroutines.CoroutineScope
@@ -23,6 +26,29 @@ class MainActivity : AppCompatActivity() {
         CoroutineScope(Dispatchers.IO).launch {
             val result = repositoryManager.getContacts()
             Timber.d("MainActivity_TAG: onCreate: contacts: ${result.size}")
+        }
+
+        findViewById<Button>(R.id.btnAdd).setOnClickListener {
+            onAddClicked()
+        }
+    }
+
+    private fun onAddClicked() {
+        Timber.d("MainActivity_TAG: onAddClicked: ")
+        val name = findViewById<TextInputEditText>(R.id.tvName).text.toString()
+        val lastName = findViewById<TextInputEditText>(R.id.tvLastName).text.toString()
+        val genre = findViewById<TextInputEditText>(R.id.tvGenre).text.toString()
+        val age = findViewById<TextInputEditText>(R.id.tvAge).text.toString()
+
+        val contact = Contact(
+            name = name,
+            lastName = lastName,
+            genre = genre,
+            age = age.toIntOrNull() ?: 0
+        )
+
+        CoroutineScope(Dispatchers.IO).launch {
+            repositoryManager.addContact(contact)
         }
     }
 }
